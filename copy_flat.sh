390a0b1b73d94ee690ed5fcdf62206b33dcd28f3
IvaIlia <140815750+IvaIlia@users.noreply.github.com>
2024-04-22 13:14:20 +0300

    Add file

---
+++ copy_flat.sh
@@ -0,0 +1,15 @@
+
+
+input_dir=$1
+output_dir=$2
+
+mkdir -p "$output_dir"
+
+find "$input_dir" -type f -print0 | while IFS= read -r -d '' file; do
+    base_name=$(basename -- "$file")
+    unique_hash=$(echo "$file" | md5sum | cut -d' ' -f1)
+    new_filename="${base_name%.*}_$unique_hash.${base_name##*.}"
+    cp "$file" "$output_dir/$new_filename"
+done
+
+
