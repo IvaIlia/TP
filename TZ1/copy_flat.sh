14e018dd41076965a914b77ee855a1172a2c4f32
IvaIlia <140815750+IvaIlia@users.noreply.github.com>
2024-06-08 14:36:09 +0300

    Update copy_flat.sh

--- TZ1/copy_flat.sh
+++ TZ1/copy_flat.sh
@@ -1,24 +1,11 @@
-#!/bin/bash
+input_dir=$1
+output_dir=$2
 
-# Принимаем входной и выходной каталоги из аргументов командной строки
-input_dir="$1"
-output_dir="$2"
-
-# Создаем выходной каталог, если он не существует
 mkdir -p "$output_dir"
 
-# Находим все файлы внутри входного каталога (включая подкаталоги) и обрабатываем их
 find "$input_dir" -type f -print0 | while IFS= read -r -d '' file; do
-    # Извлекаем имя файла без пути
     base_name=$(basename -- "$file")
-    # Извлекаем расширение файла
-    extension="${base_name##*.}"
-    # Извлекаем имя файла без расширения
-    filename="${base_name%.*}"
-    # Вычисляем уникальный хеш MD5 для содержимого файла
-    unique_hash=$(md5sum <<< "$file" | cut -d' ' -f1)
-    # Создаем новое имя файла, добавляя уникальный хеш к оригинальному имени
-    new_filename="${filename}_$unique_hash.$extension"
-    # Копируем файл в выходной каталог с новым именем
+    unique_hash=$(echo "$file" | md5sum | cut -d' ' -f1)
+    new_filename="${base_name%.*}_$unique_hash.${base_name##*.}"
     cp "$file" "$output_dir/$new_filename"
 done
